9fe48dce81db06be144a31ba2f6cb365c2b97472
Nicholas Chudinov <chudnick@hotmail.com>
2018-05-01 17:58:19 -0400

    ..
    .

--- results.php
+++ results.php
@@ -1,112 +1,93 @@
-<!doctype html>
-<html lang="en">
-	<head>
-	<meta charset="utf-8" />
-	<title>Loan Payments Calculator - Mortgage Calculator</title>
-	<meta name="description" content="Free online loan payment calculator and personal loan calculator, use our free tools to calculate your loan or finincing payments according to your interest rate, amount and term" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-	<link type="text/css" href="css/bootstrapXP.css" rel="stylesheet" />
-	<link type="text/css" href="css/mycss-v2.css" rel="stylesheet" />
-	<!-- <link type="text/css" href="css/mycss-v2.css" rel="stylesheet" /> -->
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700" rel="stylesheet">
-	<!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
-	<script>
-		function CheckMobileNav () {
-			var isMobile = false; //initiate as false
-			var displayProperty = document.getElementById("navToggle").style.display;
-// device detection
-			if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
-    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) {
-    isMobile = true;
+<?php
+/*V 6.0 Feb 20th 2017
+	- New analytics ajax system
+V 5.1 December 30th 2015
+	-DB Conection is always closed straight from the view, no need to close in footer
+V 5.0 September 07 2015
+	- New Session structure for Shopping carts and sites with sessions in _session_settings.php
+/*V. August 12 2015
+ - New Stats Tracking
+ */
+include_once('_settings.php');
+startSession();
+
+//Preview in Developer Mode
+if(isset($dev) && $dev==true){
+	$_SESSION['dev'] = true;
+	$_SESSION['rc'] = true;
+	header('location: '.str_replace("?dev=true","",$thisURL));
 }
-			if (isMobile == true){
-				if (document.getElementById("navToggle").style.display == "" || document.getElementById("navToggle").style.display == null) {
-					document.getElementById("navToggle").style.display = "none";
+//Prevent Right Click
+if(isset($RC) && $RC==true){
+	$_SESSION['rc'] = true;
+	header('location: '.str_replace("?RC=true","",$thisURL));
+}
+if($under_construction && !isset($_SESSION['dev'])){
+	if(file_exists($theme_path.'_splash.php')){
+		include_once($theme_path.'_splash.php');
+	}else{
+		include_once('_splash.php');
+	}
+}else{
+
+	if(!in_array($thisPage,$no_headers)){
+		include_once($theme_path.'_header.php');
+		include_once($theme_path.$thisPage.".php");
+		//Tracking 
+		if(isset($thisPage)){
+		?>
+		<script>
+		$(window).on("load", function() {
+			// weave your magic here.
+			$.ajax({
+				url:'/ajax/analytics.php',
+				type:'POST',
+				data:{
+					pageid:'<?php echo $thisPage; ?>',
+					trackpage:'<?php echo $request; ?>',
+					ln:'<?php echo LANG; ?>'
+				},
+				success:function(response){
+					console.log(response);
 				}
-			}
-		}
+			});					
+		});
+		</script>
+		<script>
+			$(document).ready(function(){
+				//Fixes for Font Awesome Upgrade 4 -> 5
+				// C
+				$('.fa-comments-o').removeClass('fa').addClass('fas').addClass('fa-comments').removeClass('fa-comments-o');//comments
+				$('.fa-connectdevelop').removeClass('fa').addClass('fab'); //connect develop
+				// F
+				$('.fa-facebook').removeClass('fa').addClass('fab'); //facebook
+				$('.fa-facebook-f').removeClass('fa').addClass('fab'); //facebook
+				$('.fa-facebook-square').removeClass('fa').addClass('fab'); //facebook
+				//G
+				$('.fa-file-text-o').removeClass('fa').addClass('far').addClass('fa-file-alt').removeClass('fa-file-text-o'); //file
+				$('.fa-google-plus').removeClass('fa').addClass('fab'); //Google
+				// I
+				$('.fa-instagram').removeClass('fa').addClass('fab'); //instagram
+				// L
+				$('.fa-lightbulb-o').removeClass('fa').addClass('far').addClass('fa-lightbulb').removeClass('fa-lightbulb-o'); //light bulb
+				$('.fa-linkedin').removeClass('fa').addClass('fab'); //Linked In
+				// P
+				$('.fa-paypal').removeClass('fa').addClass('fab'); //paypal
+				$('.fa-pencil-square-o').removeClass('fa').addClass('far').addClass('fa-edit').removeClass('fa-pencil-square-o'); //edit
+				$('.fa-picture-o').removeClass('fa').addClass('far').addClass('fa-image').removeClass('fa-picture-o'); //picture / image
+				$('.fa-pinterest-p').removeClass('fa').addClass('fab'); //Pinterest
+				// R
+				$('.fa-refresh').removeClass('fa').addClass('fas').addClass('fa-sync').removeClass('fa-refresh'); //refresh - captchaXP
+				// T
+				$('.fa-trash-o').removeClass('fa').addClass('far').addClass('fa-trash-alt').removeClass('fa-trash-o');//trash
+				$('.fa-twitter').removeClass('fa').addClass('fab'); //twitter
+				$('.fa-twitter-square').removeClass('fa').addClass('fab'); //twitter
+				// V
+				$('.fa-video-camera').removeClass('fa').addClass('fas').addClass('fa-video').removeClass('fa-video-camera'); //video camera
+				
+			});
+		</script>
 
-		function navToggle(x) {
-    		x.classList.toggle("change");
-
-			var displayProperty = document.getElementById("navToggle").style.display;
-			var visibilityProperty = document.getElementById("navToggle").style.visibility;
-
-
-			if (document.getElementById("navToggle").style.display === "none") {
-				document.getElementById("navToggle").style.display = "block";
-				document.getElementById("navToggle").style.visibility = "visible";
-				document.getElementById("navToggle").style.transition = "visibility 1s ease 0";
-				document.getElementById("navToggle").style.opacity = "1";
-				document.getElementById("navToggle").style.transition = "opacity 1s ease 0";
-			} else {
-				document.getElementById("navToggle").style.display = "none";
-				document.getElementById("navToggle").style.visibility = "hidden";
-				document.getElementById("navToggle").style.transition = "visibility 1s ease 0";
-				document.getElementById("navToggle").style.opacity = "0";
-				document.getElementById("navToggle").style.transition = "opacity 1s ease 0";
-			}
-		}
-	</script>
-</head>
-
-	<body onLoad="CheckMobileNav ()">
-<div class="fullContainer">
-		<div>
-		<header>
-				<div class="container">
-				<!-- Logo goes here -->
-				<div class="col-12">
-						<div class="col-4"> <a href="index.html"><img alt="" class="logo col-1 floatLeft" src="img/svg/logo.svg" />
-						<div class="col-6 floatLeft"> <span class="logo-name">Loan Payment</span> <span class="logo-name-sec">Calculator</span> </div></a>
-					</div>
-					<div class="floatRight">
-						<div class="mob-nav-icon tablet mobile hamburger" onclick="navToggle(this)">
-							<div class="bar1"></div>
-							<div class="bar2"></div>
-							<div class="bar3"></div>
-						</div>
-					</div>
-						<div class="col-1 floatRight">
-						<div class="dropdown desktop"> <span><img class="imgLang" src="img/UK_Flag.png" alt=""></span>
-								<div class="dropdown-content">
-								<div><img class="imgLang" src="img/UK_Flag.png" alt=""></div>
-								<div><img class="imgLang" src="img/Flag_of_France.png" alt=""></div>
-							</div>
-							</div>
-					</div>
-					</div>
-			</div>
-			</header>
-	</div>
-		<div class="fullContainer navBackground clearFloat">
-		<div>
-				<div class="container">
-				<nav class="mainNav">
-						<!-- Nav goes here -->
-						<ul id="navToggle">
-							<li class="tablet mobile reset-container col-mob-12">
-								<a class="span-link col-mob-4" href="#EN">EN</a>
-								<a class="span-link col-mob-4" href="#FR">FR</a>
-								<a class="span-link col-mob-4" href="#ES">ES</a>
-							</li>
-						<li><a href="calc-page.html">Mortgage</a></li>
-						<li><a href="calc-page.html">School Loans</a></li>
-						<li><a href="calc-page.html">Bank Rates</a></li>
-						<li><a href="calc-page.html">Credit Cards</a></li>
-						<li><a href="calc-page.html">Personal Loans</a></li>
-						<li><a href="calc-page.html">Car Loans</a></li>
-						<li><a href="calc-page.html">Business Loans</a></li>
-						<li><a href="calc-page.html">Equipement Loans</a></li>
-						<li><a href="blog.html">Blog</a></li>
-					</ul>
-					</nav>
-			</div>
-			</div>
-	</div>
 		<div class="fullContainer">
 		<div class="fullContainerCalcMortgage">
 				<!-- Banner background image one goes here -->
@@ -183,6 +164,12 @@
 		    	var myChartJs = document.getElementById("myChartLine").getContext("2d");
 					var amount = <?php $amount = $_GET["hAmmount"]; echo $amount;?>;
 					var interest = 12000;
+					var monthly;
+					var interestRate;
+					var interestMonthly;
+					var termPeriod = <?php $term = $_GET[""]; echo $term;?>
+					var months[] = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+					
 
 					var statsChart = new Chart(myChartJs, {
 						type:'line',
@@ -386,12 +373,44 @@
             </div>
 		<div class="ad1 col-4 desktop floatRight"><!-- half page ad goes here--></div>
 	</div>
-<div class="clearFloat">
-		<footer class="centered">
-		<p>© 2018 LoanPaymentsCalculator.com<br>
-				Terms and Conditions | Privacy Policy | Accessibility</p>
-	</footer>
-	</div>
-</div>
-</body>
-</html>
+		<?php }
+		include_once($theme_path.'_footer.php');
+		
+
+		$endMem = round(memory_get_usage()/1024,2);
+		$pickMem = round(memory_get_peak_usage()/1024,2);
+		echo "
+		<!--
+		Referral: ".$_SESSION["origURL"]."
+		BUILT WITH ROUNDPLUG copyright 2008-".date("Y")." Multimedia Solutions and Consulting
+		www.multimediaxp.com
+		Country Code Provided by Cloudflare: $country_code";
+		if($ipLocation != null){
+		echo "
+		IP Geolocation ".$_SERVER['REMOTE_ADDR'].": ".$ipLocation->getCountry()." ".$ipLocation->getRegion()." ".$ipLocation->getCity();
+		}
+		echo "
+		User Info:
+			- OS: $os $osversion
+			- Browser: $browser_name V. $browser_version
+		Server Performance:
+			-	Initial Memory Usage: $initMem Mb
+			-	Final Memory Usage: $endMem Mb,
+			-	Memory used in this process: ".($endMem-$initMem)." Mb,
+			-	Pick Memory Use: $pickMem Mb
+		-->";
+
+	}else{
+		include_once($theme_path.$thisPage.".php");
+	}
+	
+}
+if($hasDatabase){
+	$connection->close();
+	if(isset($pl_db_username)){
+		if($centrisDB->is_connected()){
+			$centrisDB->close();
+		}
+	}
+}
+ ?>
